ca46f0abd645e78208ebc26c04e0306ad7b66aee
Swedrft <104517601+Swedrft@users.noreply.github.com>
2024-01-19 13:11:21 +0100

    Update Jump.cs
    improved movement

--- Jump.cs
+++ Jump.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     public float jumpforce;
+    public float move;
+    public float DirR;
+    public float DirL;
+    public float back;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +21,16 @@ public class Jump : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.W)){
             GetComponent<Rigidbody>().velocity = Vector3.up * jumpforce;
+        
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            GetComponent<Rigidbody>().velocity = Vector3.back * back;
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+           GetComponent<Rigidbody>().velocity = Vector3.left * DirL;
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+           GetComponent<Rigidbody>().velocity = Vector3.right * DirR;
         }
     }
 }
